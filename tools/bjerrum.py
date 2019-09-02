4965ee84d2162dfcbe9bb741dc2f16bdf22458dd
Debesh <debesh.mandal13@imperial.ac.uk>
2019-09-02 11:36:04 +0100

    addition of tools for salt concentration calculations

---
+++ tools/bjerrum.py
@@ -0,0 +1,18 @@
+from math import pi
+e = 1.602e-19
+eps0 = 8.85e-12
+kb = 1.38e-23
+
+def bjerrum(epsr, T, reduced=False):
+    if reduced == True:
+        k = 1
+        eps = epsr
+        top = 1
+    else:
+        k = kb
+        eps = epsr * eps0
+        top = e ** 2
+    bottom = eps * k * T * 4 * pi
+    length = top/bottom
+    print "The Bjerrum length is {}".format(length)
+    return length
---
+++ tools/debye.py
@@ -0,0 +1,14 @@
+from bjerrum import bjerrum
+from math import pi
+
+def I(salts):
+    strength = 0
+    for salt in salts:
+        strength += salt[0] ** 2 * salt[1]
+    return strength
+
+def debye(salts, epsr=74.5, T=310, reduced=False):
+    length = 8 * pi * bjerrum(epsr, T, reduced=False) * I(salts) * avo * 1e3
+    length = length ** -0.5
+    print "The Debye length is {}".format(length)
+    return length
