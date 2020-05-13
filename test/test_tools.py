83b6a59526b48f6e32776402f445a15b016db595
Debesh-XPS <debesh.mandal@hotmail.com>
2020-05-13 21:40:13 +0100

    updated tools testing

---
+++ test/test_tools.py
@@ -0,0 +1,25 @@
+from starpolymers.tools.bjerrum import *
+from starpolymers.tools.cylinders import *
+from starpolymers.tools.debye import *
+from starpolymers.tools.design import *
+from starpolymers.tools.neutraliser import *
+from starpolymers.tools.plotter import *
+
+def test_bjerrum():
+    assert True
+    
+def test_cylinders():
+    assert True
+    
+def test_debye():
+    assert True
+    
+def test_design():
+    assert True
+    
+def test_neutraliser():
+    assert True
+    
+def test_plotter():
+    assert True
+    
