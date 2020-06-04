bcbbb773573993c8bb01467dc91cee0762eb2467
Debesh-XPS <debesh.mandal@hotmail.com>
2020-06-04 18:20:38 +0100

    deployment complete

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="starpolymers",
-    version="1.0.1",
+    version="1.1.0",
     author="Debesh Mandal",
     description="Package for creating polymers for simulation",
     long_description=long_description,
