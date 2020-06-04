e72007b3070d009b3b949b0da1962d5e459cae51
Debesh-XPS <debesh.mandal@hotmail.com>
2020-06-04 18:07:35 +0100

    python requires updated

--- setup.py
+++ setup.py
@@ -16,5 +16,6 @@ setuptools.setup(
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
+    python_requires='~=2.7',
 )
