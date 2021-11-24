861f3c2b1a5df16adb76853cc5350293eb45986a
Debesh Mandal <debesh.mandal@hotmail.com>
2021-11-24 17:53:50 +0000

    implemented different config file styles - closes #18

--- starpolymers/io/configuration.py
+++ starpolymers/io/configuration.py
@@ -1,27 +1,139 @@
 from ..generators.system import System
 import datetime
+import time
 
 import pandas as pd
 
+STYLES = [
+    'atomic',
+    'bond',
+    'angle',
+    'full',
+    'molecular',
+    'charge',
+]
+
 class ConfigFile():
-    def __init__(self, system, comment='', include_angles: bool = True):
+    def __init__(
+        self,
+        system: System,
+        comment: str = '',
+        style: str = 'full',
+        include_bonds: bool = True,
+        include_angles: bool = True,
+        include_dihedrals: bool = False,
+        include_charges: bool = False,
+    ):
+        # important attributes
         self._system = system
         self.comment = comment
-        self.include_angles = include_angles
+        self.style = style
 
-    def write(self, fname='config.dat', angles: bool = True):
-        _include_angles = self.include_angles
-        if not angles:
+        # list of things to include
+        # note that style will dynamically EXCLUDE some of these
+        self.include_bonds = include_bonds
+        self.include_angles = include_angles
+        self.include_dihedrals = include_dihedrals
+        self.include_charges = include_charges
+
+        self.implement_style()
+
+    def implement_style(self, style: str = None):
+        """Changes all obj.include_* boolean storage units to ensure
+        only the proper parts of the system are included"""
+        if style:
+            self.style = style
+
+        def atomic():
+            self.include_bonds = False
             self.include_angles = False
+            self.include_dihedrals = False
+            self.include_charges = False
+            return
+
+        def bond():
+            # skip bonds - defined in class
+            self.include_angles = False
+            self.include_dihedrals = False
+            self.include_charges = False
+            return
+
+        def angle():
+            # skip bonds - defined in class
+            # skip angles - defined in class
+            self.include_dihedrals = False
+            self.include_charges = False
+            return
+
+        def molecular():
+            # skip bonds - defined in class
+            # skip angles - defined in class
+            # skip dihedrals - defined in class
+            self.include_charges = False
+            return
+
+        def dihedral():
+            # skip bonds - defined in class
+            # skip angles - defined in class
+            # skip dihedrals - defined in class
+            self.include_charges = False
+            raise NotImplementedError
+            return
+
+        def full():
+            # skip bonds - defined in class
+            # skip angles - defined in class
+            # skip dihedrals - defined in class
+            self.include_charges = True
+            return
+
+        def charge():
+            self.include_bonds = False
+            self.include_angles = False
+            self.include_dihedrals = False
+            self.include_charges = True
+            return
+
+        # create hash-table of style functions
+        style_functions = {
+            'atomic': atomic,
+            'bond': bond,
+            'angle': angle,
+            'dihedral': dihedral,
+            'full': full,
+            'molecular': molecular,
+            'charge': charge
+        }
+
+        # choose function and call
+        style_functions[self.style]()
+
+        return
+
+    def write(
+        self,
+        fname='lammps.main.conf',
+        bonds: bool = None,
+        angles: bool = None,
+        dihedrals: bool = None,
+        style: str = None,
+    ):
+
+        if style:
+            self.implement_style(style=style)
+
         with open(fname, 'w') as f:
             f.write(self.comments)
             f.write(self.header)
             f.write(self.masses)
             f.write(self.atoms)
-            f.write(self.bonds)
-            if self.include_angles:
+            if bonds or self.include_bonds:
+                f.write(self.bonds)
+            if angles or self.include_angles:
                 f.write(self.angles)
-        self.include_angles = _include_angles
+            if dihedrals or self.include_dihedrals:
+                raise NotImplementedError
+
 
     @property
     def comments(self):
@@ -63,15 +175,26 @@ class ConfigFile():
         atoms = '\nAtoms\n\n'
         table = self._system.atoms
         for i in range(len(table)):
-            atoms += "{} {} {} {} {} {} {}\n".format(
-                i + 1,
-                table['mol'][i],
-                table['type'][i],
-                table['q'][i],
-                table['x'][i],
-                table['y'][i],
-                table['z'][i]
-            )
+            if self.include_charges:
+                atoms += "{} {} {} {} {} {} {}\n".format(
+                    i + 1,
+                    table['mol'][i],
+                    table['type'][i],
+                    table['q'][i],
+                    table['x'][i],
+                    table['y'][i],
+                    table['z'][i]
+                )
+            else:
+                atoms += "{} {} {} {} {} {}\n".format(
+                    i + 1,
+                    table['mol'][i],
+                    table['type'][i],
+                    table['x'][i],
+                    table['y'][i],
+                    table['z'][i]
+                )
+
         return atoms
 
     @property
--- test/io/test_configuration.py
+++ test/io/test_configuration.py
@@ -1,7 +1,35 @@
-import starpolymers.io.configuration
+from pathlib import Path
 
-def test_configuration():
+from starpolymers.io.configuration import *
+
+from starpolymers import ConfigFile
+from starpolymers import System
+
+from starpolymers.molecules import LinearPolyelectrolyte
+
+FOLDER = Path(__file__).parent
+
+def test_configuration(delete: bool = True):
+    item = {
+        'molecule': 'dna',
+        'lam': 10
+    }
+    molecule = LinearPolyelectrolyte(item)
+    system = System(10., molecules=[molecule])
+    for style in STYLES:
+        config_file = ConfigFile(system, style=style)
+
+        OUT = FOLDER / f'lammps.{style}.conf'
+        OUT.unlink(missing_ok=True)
+        config_file.write(OUT)
+
+        if delete:
+            OUT.unlink()
+
     return
 
 if __name__ == '__main__':
-    test_configuration()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--delete', action='store_true')
+    test_configuration(parser.parse_args().delete)
